b3803b99e90b1e818b4223f3cfa426b1a2b6f047
Priya Gupta <er.priyagupta123@gmail.com>
2023-04-05 21:05:31 -0700

    port changed

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const path = require('path');
-const store = require('./store/datastore');
-const initialStoreData = require('./store/data');
-const Musician = require('./models/musician');
-const musicianRoutes = require('./routes/musician');
+const express = require('express')
+const path = require('path')
+const store = require('./store/datastore')
+const initialStoreData = require('./store/data')
+const Musician = require('./models/musician')
+const musicianRoutes = require('./routes/musician')
 
-const app = express();
-const port = process.env.PORT || 3001;
+const app = express()
+const port = process.env.PORT || 8080
 
 // include routes
-app.use('/musician', musicianRoutes);
+app.use('/musician', musicianRoutes)
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Index route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
+})
 
 // initialize store
-const musician = new Musician(store);
-musician.initStore(initialStoreData);
-app.locals.musician = musician;
+const musician = new Musician(store)
+musician.initStore(initialStoreData)
+app.locals.musician = musician
 
 // start server
 const server = app.listen(port, () => {
-  console.log("Server started on port " + port);
-});
+  console.log('Server started on port ' + port)
+})
 
-module.exports = server;
+module.exports = server
